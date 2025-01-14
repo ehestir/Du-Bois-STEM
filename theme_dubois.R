e0f6826ca608bb1923944449d27d1e4c9b91b034
Charlie Eaton <eaton.charlie@gmail.com>
2025-01-14 15:16:52 -0800

    simply dubois theme

--- theme_dubois.R
+++ theme_dubois.R
@@ -1,11 +1,16 @@
   theme_dubois <- function() { 
         theme(
-            text = element_text(face = "bold"), plot.background = element_rect(fill = "antiquewhite2",
-        color = "antiquewhite2"), plot.title = element_text(hjust = 0.5, face = "bold"),
-        plot.subtitle = element_text(hjust = 0.5, size = 9), panel.background = element_blank(),
-        panel.grid.major.x = element_blank(), panel.grid.minor = element_blank(), 
-        panel.grid.major.y = element_blank(), axis.text.y = element_blank(), 
-        axis.ticks = element_blank(), axis.title = element_blank(),
-        axis.text.x=element_text(size=12, face="bold")
+            text = element_text(face = "bold"),
+        legend.position = "none",
+        axis.text.y = element_text(size = 12),
+        plot.title = element_text(hjust = 0.5, size = 18),
+        plot.subtitle = element_text(hjust = 0.5, size = 13),
+        plot.title.position = "plot",
+        plot.background = element_rect(fill = 'antiquewhite2'),
+        panel.background = element_blank(),
+        panel.grid = element_blank(), # Removes both major and minor gridlines
+        axis.ticks = element_blank(),
+        axis.title = element_blank(), # Combines blanking of x and y titles
+        axis.text.x = element_blank() # Removes x-axis text
         )
                               }
